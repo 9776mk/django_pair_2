d34ece114ac75627e711408d01ead25da9fcd8dc
hobin49 <hobin505@gmail.com>
2022-10-07 10:24:00 +0900

    first

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Movie
+from .forms import MovieForm
 
 # Create your views here.
+
+
+def main(request):
+    return render(request, "movies/main.html")
