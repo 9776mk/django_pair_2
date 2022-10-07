a76220a5b0f0b6a77f2cd911fb49939a8ebb021a
Gkhy <syan1022@naver.com>
2022-10-07 11:35:29 +0900

    detaildelete

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path  
-from . import views 
+from django.urls import path
+from . import views
 
 
 app_name = "movies"
 
-urlpatterns = [ 
+urlpatterns = [
     path("", views.main, name="main"),
     path("index/", views.index, name="index"),
+    path("<int:pk>/", views.detail, name="detail"),
+    path("<int:pk>/delete/", views.delete, name="delete"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -8,10 +8,22 @@ from .forms import MovieForm
 def main(request):
     return render(request, "movies/main.html")
 
+
 def index(request):
     movies = Movie.objects.all()
-    context = {
-        "movies": movies
-    }
+    context = {"movies": movies}
 
     return render(request, "movies/index.html", context)
+
+
+def detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+
+    context = {"movie": movie}
+
+    return render(request, "movies/detail.html", context)
+
+
+def delete(request, pk):
+    Movie.objects.get(pk=pk).delete()
+    return redirect("movies:index")
